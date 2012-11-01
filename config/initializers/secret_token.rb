ccbc77d4333f1276499a2137467f7465a823c5e3
Alberto Mignani <albertomignani@gmail.com>
2012-11-01 12:16:26 +0100

    styled user forms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '9f8e73d3a3ce1c40da072bb7aaca09747dfeb88b165a50285c67e1783bb0d9dec7c0eff00fe70a08fd582b1de447993671d43e8d3ce626e07a445439f54097e6'
---
+++ simple_form/lib/simple_form/version.rb
@@ -0,0 +1,3 @@
+module SimpleForm
+  VERSION = "0.0.1"
+end
