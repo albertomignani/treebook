392bdea5ba366d0c2fa547046a61a7f36bc1304f
Alberto Mignani <albertomignani@gmail.com>
2012-11-07 23:25:17 +0100

    Added gravatar

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -18,4 +18,11 @@ class ProfilesControllerTest < ActionController::TestCase
     assert_not_empty assigns(:statuses)
   end
 
+  test "only shows the correct user's statuses" do
+    get :show, id: users(:albertomignani).profile_name
+    assigns(:statuses).each do |status|
+      assert_equal users(:albertomignani), status.user
+    end
+  end
+
 end
